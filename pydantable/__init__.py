bfaf20ed58f0e01d4a9905773628f57c4ed7c673
Odos Matthews <odosmatthews@odoss-mbp.lan>
2024-08-14 16:08:55 -0400

    add read methods

---
+++ pydantable/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.0'
+
+from pydantable.table import BaseTableModel
--- pydantable/table.py
+++ pydantable/table.py
@@ -1,42 +1,66 @@
 from __future__ import annotations
 import typing as _t
 import pydantic as _pydantic
+import tinytable as _tt
+import tinytable.csv
+import tinytim.rows
 
-class IterTable:
-    def __init__(self, table: Table) -> None:
-        self.table: Table = table
-        self.i = 0
-
-    def __next__(self) -> _pydantic.BaseModel:
-        if self.i >= len(self.table):
-            raise StopIteration
-        row: dict[str, _t.Any] =  {
-            col: values[self.i]
-                for col, values in self.table.data.items()
-        }
-        return self.table.model.model_construct(**row)
+from pydantable import errors
 
 
-class Table:
-    def __init__(
-        self,
-        data: dict[str, _t.Any],
-        model: _pydantic.BaseModel
-    ) -> None:
-        self.data: dict[str, _t.Any] = data
-        self.model: _pydantic.BaseModel = model
+def validate_row(
+    row: dict,
+    model: _t.Type[_pydantic.BaseModel]
+) -> dict:
+    return dict(model.model_validate(dict(row)))
 
-    def columns(self) -> list[str]:
-        return list(self.data.keys())
 
-    def __len__(self) -> int:
-        column_names: list[str] = self.columns()
-        if len(column_names) == 0:
-            return 0
-        return len(self.data[column_names[0]])
+def validate_table(
+    table: _tt.Table,
+    model: _t.Type[_pydantic.BaseModel]
+) -> _tt.Table:
+    out_table: _tt.Table = table.copy()
+    validation_errors: list[dict] = []
+    errored = False
+    for i, row in table.iterrows():
+        try:
+            validated_row: dict = validate_row(dict(row), model)
+            if not errored:
+                out_table[i] = validated_row
+        except _pydantic.ValidationError as e:
+            validation_errors.extend(e.errors())
+            errored = True
+    if validation_errors:
+        grouped_errors: list[dict] = errors.group_errors(validation_errors)
+        raise errors.ValidationErrors(grouped_errors)
+    return out_table
+    
 
-    def __iter__(self) -> IterTable:
-        return IterTable(self)
-
-    def validate(self):
-        ...
+class BaseTableModel(_pydantic.BaseModel):
+    # TODO: Add __init__ for reading dict
+    
+    @classmethod
+    def read_csv(cls, path: str) -> _tt.Table:
+        tbl: _tt.Table = _tt.read_csv(path)
+        return validate_table(tbl, cls)
+    
+    @classmethod
+    def read_dict(cls, d: dict[str, _t.Sequence]) -> _tt.Table:
+        tbl = _tt.Table(d)
+        return validate_table(tbl, cls)
+    
+    @classmethod
+    def read_excel(cls, path: str, sheet_name: str | None = None) -> _tt.Table:
+        tbl: _tt.Table = _tt.read_excel(path, sheet_name)
+        return validate_table(tbl, cls)
+    
+    @classmethod
+    def read_sqlite(cls, path: str, table_name: str) -> _tt.Table:
+        tbl: _tt.Table = _tt.read_sqlite(path, table_name)
+        return validate_table(tbl, cls)
+        
+    @classmethod
+    def read_csv_chunks(cls, path: str, chunksize: int) -> _t.Generator[_tt.Table, None, None]:
+        for d_chunk in tinytable.csv.chunk_csv_file(path, chunksize):
+            tbl = _tt.Table(d_chunk)
+            yield validate_table(tbl, cls)
